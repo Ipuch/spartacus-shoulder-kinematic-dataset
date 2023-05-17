0b14a1e3ee8124520cbc41783e64a07f0cf1ba5c
Ipuch <pierre.puchaud@umontreal.ca>
2023-05-17 15:56:07 -0400

    trying to install the module

---
+++ setup.py
@@ -0,0 +1,21 @@
+""" Setup.py file """
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="shoulder",
+    version="0.0.1",
+    author="Ipuch, ANaaim, FMoissenet",
+    author_email="",
+    description="Shoulder data treatment",
+    long_description=long_description,
+    packages=[
+        ".",
+        "shoulder",
+        "examples",
+    ],
+    python_requires=">=3.10",
+    zip_safe=False,
+)
