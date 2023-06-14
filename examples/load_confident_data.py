cf7440b06d0fefcfad7a1b9679130b2f8aeba1bd
Ipuch <pierre.puchaud@umontreal.ca>
2023-06-14 18:56:29 -0400

    extra checks and prints

--- examples/load_confident_data.py
+++ examples/load_confident_data.py
@@ -44,6 +44,7 @@ def load_confident_data(df: pd.DataFrame, print_warnings: bool = False)-> pd.Dat
     for i, row in df.iterrows():
         # print(row.article_author_year)
 
+        # Checking all the segments
         for segment, is_isb, segment_enum in zip(tested_segments, segment_is_isb, Segment):
             # print(segment)
             # if any of each is nan, skip
@@ -102,24 +103,50 @@ def load_confident_data(df: pd.DataFrame, print_warnings: bool = False)-> pd.Dat
         child_segment = segment_str_to_enum(row.child)
         child_columns = get_segment_columns(child_segment)
 
-        usable, callback_function = check_biomech_consistency(
-            parent_segment=BiomechCoordinateSystem.from_biomech_directions(
+        # check database if nan in one the segment of the joint
+        if not isinstance(row[parent_columns[0]], str) or not isinstance(row[parent_columns[1]], str) or not isinstance(row[parent_columns[2]], str):
+            if np.isnan(row[parent_columns[0]]) or np.isnan(row[parent_columns[1]]) or np.isnan(row[parent_columns[2]]):
+                if print_warnings:
+                    print("WARNING : nan in parent segment")
+                continue
+        if not isinstance(row[child_columns[0]], str) or not isinstance(row[child_columns[1]], str) or not isinstance(row[child_columns[2]], str):
+            if np.isnan(row[child_columns[0]]) or np.isnan(row[child_columns[1]]) or np.isnan(row[child_columns[2]]):
+                if print_warnings:
+                    print("WARNING : nan in child segment")
+                continue
+
+        parent_biomech_sys = BiomechCoordinateSystem.from_biomech_directions(
                 x=biomech_direction_string_to_enum(row[parent_columns[0]]),
                 y=biomech_direction_string_to_enum(row[parent_columns[1]]),
                 z=biomech_direction_string_to_enum(row[parent_columns[2]]),
                 origin=biomech_origin_string_to_enum(row[parent_columns[3]]),
                 segment=parent_segment,
-            ),
-            child_segment=BiomechCoordinateSystem.from_biomech_directions(
+            )
+        child_biomech_sys = BiomechCoordinateSystem.from_biomech_directions(
                 x=biomech_direction_string_to_enum(row[child_columns[0]]),
                 y=biomech_direction_string_to_enum(row[child_columns[1]]),
                 z=biomech_direction_string_to_enum(row[child_columns[2]]),
                 origin=biomech_origin_string_to_enum(row[child_columns[3]]),
                 segment=child_segment,
-            ),
+            )
+
+        usable, callback_function = check_biomech_consistency(
+            parent_segment=parent_biomech_sys,
+            child_segment=child_biomech_sys,
             joint=bjoint,
         )
+
         if not usable:
+            if print_warnings:
+                print("WARNING : inconsistency in the dataset")
+                print(row.joint, row.article_author_year)
+                print("detected :", bjoint.joint_type)
+                print("detected parent segment :", parent_segment)
+                parent_biomech_sys.__print__()
+                print("detected child segment :", child_segment)
+                child_biomech_sys.__print__()
+                print("detected joint coordinate system :", bjoint.euler_sequence)
+                print("callback function :", callback_function)
             continue
         # add the callback function to the dataframe
         row.callback_function = callback_function
@@ -133,5 +160,5 @@ def load_confident_data(df: pd.DataFrame, print_warnings: bool = False)-> pd.Dat
 # open the file only_dataset_raw.csv
 df = pd.read_csv(DatasetCSV.CLEAN.value)
 print(df.shape)
-df = load_confident_data(df)
+df = load_confident_data(df, print_warnings=True)
 print(df.shape)
