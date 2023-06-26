3e519760f0b1fec59639d4c9173f4068cee5d3b2
Ipuch <pierre.puchaud@umontreal.ca>
2023-06-26 09:34:18 -0400

    black commit on the fly :)

--- examples/load_dataset.py
+++ examples/load_dataset.py
@@ -7,7 +7,6 @@ from shoulder import DatasetCSV, biomech_direction_string_to_enum, BiomechCoordi
 df = pd.read_csv(DatasetCSV.CLEAN.value)
 # for each row verify the ISB convention
 for i, row in df.iterrows():
-
     print(row.article_author_year)
 
     tested_segments = [
@@ -26,8 +25,10 @@ for i, row in df.iterrows():
     for segment, is_isb in zip(tested_segments, segment_is_isb):
         print(segment)
         # if any of each is nan, skip
-        if isinstance(row[segment[0]], float) or isinstance(row[segment[1]], float) or isinstance(
-            row[segment[2]], float
+        if (
+            isinstance(row[segment[0]], float)
+            or isinstance(row[segment[1]], float)
+            or isinstance(row[segment[2]], float)
         ):
             if np.isnan(row[segment[0]]) or np.isnan(row[segment[1]]) or np.isnan(row[segment[2]]):
                 print(segment, "is nan")
