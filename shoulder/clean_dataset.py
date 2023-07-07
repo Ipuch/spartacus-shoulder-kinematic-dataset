97270b31fa9fcf118b8bcb4401ee1bede6a22ccb
Ipuch <pierre.puchaud@umontreal.ca>
2023-07-07 10:11:45 -0400

    data set update !

--- shoulder/clean_dataset.py
+++ shoulder/clean_dataset.py
@@ -4,11 +4,15 @@ from enums import DatasetCSV
 
 
 # open the file only_dataset_raw.csv
-df = pd.read_csv(DatasetCSV.RAW.value)
-# clean lines with only NaN
-df = df.dropna(how="all")
+df = pd.read_csv(DatasetCSV.RAW.value, delimiter=",")
+print(df.iloc[0, 0])
+assert df.iloc[0, 0] == "Bourne"
+assert len(df.iloc[0, :]) == 66
 # rename all the columns with only lower cases
 df.columns = [col.lower() for col in df.columns]
+# clean lines with only NaN
+df = df.dropna(how="all")
+
 # set all the "true" to True and "false" to False
 df = df.replace({"true": True, "false": False})
 # remove extra spaces in the columns with false and true
