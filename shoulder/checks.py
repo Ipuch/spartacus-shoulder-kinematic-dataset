fb8bb2006b41aa61f0cbee6beb48c18406d3fec3
Ipuch <pierre.puchaud@umontreal.ca>
2023-07-10 15:35:58 -0400

    extra note

--- shoulder/checks.py
+++ shoulder/checks.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from .enums import JointType, Segment
-from .utils import segment_str_to_enum, Joint, BiomechCoordinateSystem, get_is_isb_column, get_is_correctable_column
+from .utils import segment_str_to_enum, Joint, BiomechCoordinateSystem, get_is_isb_column, get_is_correctable_column, get_correction_column
 
 
 def check_parent_child_joint(bjoint: Joint, row: pd.Series, print_warnings: bool = False):
@@ -99,6 +99,15 @@ def check_is_isb_segment(row: pd.Series, bsys: BiomechCoordinateSystem, print_wa
     -------
     bool
         True if the segment is ISB oriented and if it is well specified in the dataset, False otherwise.
+
+    Notes
+    -----
+    This function does not check if the segment is ISB oriented, it only checks if the dataset is well specified.
+    Ex: a segment can be entirely ISB in the dataset, but if the correctable column is filled with True or False, then
+    it means it is an ISB like segment, but not exactly ISB. Because it can either be corrected or not.
+    if nan/None is given in the correctable column, then it means we don't have to apply any correction, so the segment
+    is a well-defined ISB segment.
+
     """
     is_isb = get_is_isb_column(bsys.segment)
     is_correctable_col = get_is_correctable_column(bsys.segment)
