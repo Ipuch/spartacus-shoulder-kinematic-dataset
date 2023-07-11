6e481b798bb266142e7a99a45500b1e477327aa7
Ipuch <pierre.puchaud@umontreal.ca>
2023-07-11 10:19:35 -0400

    kolz and orthonormalization #62

--- shoulder/kolz_matrices.py
+++ shoulder/kolz_matrices.py
@@ -2,7 +2,7 @@ import numpy as np
 from .enums import Correction
 
 
-def get_kolz_rotation_matrix(correction: Correction) -> np.ndarray:
+def get_kolz_rotation_matrix(correction: Correction, orthonormalize: bool = True) -> np.ndarray:
     """
     This function returns the rotation matrix for the given correction.
 
@@ -10,24 +10,26 @@ def get_kolz_rotation_matrix(correction: Correction) -> np.ndarray:
     ----------
     correction : Correction
         The correction to apply.
-
-    Source
-    ------
-    Kolz, C. W., Sulkar, H. J., Aliaj, K., Tashjian, R. Z., Chalmers, P. N., Qiu, Y., ... & Henninger, H. B. (2020).
-    Reliable interpretation of scapular kinematics depends on coordinate system definition. Gait & posture, 81, 183-190.
+    orthonormalize : bool, optional
+        If True, the rotation matrix is orthonormalized. The default is True.
 
     Returns
     -------
     np.ndarray
         The rotation matrix for the given correction.
         R_isb_local, such that a_in_isb = R_isb_local * a_in_local
+
+    Source
+    ------
+    Kolz, C. W., Sulkar, H. J., Aliaj, K., Tashjian, R. Z., Chalmers, P. N., Qiu, Y., ... & Henninger, H. B. (2020).
+    Reliable interpretation of scapular kinematics depends on coordinate system definition. Gait & posture, 81, 183-190.
     """
 
     if correction == Correction.SCAPULA_KOLZ_AC_TO_PA_ROTATION:
         # meaning a vector in AC coordinate system is expressed as:
         # a' = R * a
         # where a' is the vector expressed in the PA coordinate system
-        return np.array(
+        R = np.array(
             [
                 [0.965, 0.010, -0.263],
                 [-0.057, 0.984, 0.171],
@@ -39,7 +41,7 @@ def get_kolz_rotation_matrix(correction: Correction) -> np.ndarray:
         # meaning a vector in glenoid coordinate system is expressed as:
         # a' = R * a
         # where a' is the vector expressed in the PA coordinate system
-        return np.array(
+        R = np.array(
             [
                 [0.949, 0.010, -0.314],
                 [-0.056, 0.978, 0.200],
@@ -50,3 +52,30 @@ def get_kolz_rotation_matrix(correction: Correction) -> np.ndarray:
     else:
         raise ValueError(f"{correction} is not a valid correction. Only {Correction.SCAPULA_KOLZ_AC_TO_PA_ROTATION} "
                          f"and {Correction.SCAPULA_KOLZ_GLENOID_TO_PA_ROTATION} are valid corrections.")
+
+    return orthonormalize_matrix(R) if orthonormalize else R
+
+
+# normalize a matrix with svd and return the normalized matrix
+def orthonormalize_matrix(matrix):
+
+    # normalize the matrix
+    u, s, vh = np.linalg.svd(matrix, full_matrices=True)
+    normalized_matrix = u @ vh
+
+    # print("matrix")
+    # # print in scientific notation with e
+    # np.set_printoptions(suppress=True, formatter={'float_kind': '{:0.3e}'.format})
+    # print(matrix)
+    # print(np.linalg.det(matrix))
+    #
+    # print("normalized matrix")
+    # print(normalized_matrix)
+    # print(np.linalg.det(normalized_matrix))
+
+    # print("differences between matrix and normalized matrix")
+    # print(matrix - normalized_matrix)
+
+    return normalized_matrix
+
+
--- tests/test_kolz.py
+++ tests/test_kolz.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 from shoulder.kolz_matrices import get_kolz_rotation_matrix
@@ -5,7 +6,7 @@ from shoulder.enums import Correction
 
 
 def test_kolz():
-    R = get_kolz_rotation_matrix(Correction.SCAPULA_KOLZ_AC_TO_PA_ROTATION)
+    R = get_kolz_rotation_matrix(Correction.SCAPULA_KOLZ_AC_TO_PA_ROTATION, orthonormalize=False)
     assert R.shape == (3, 3)
     assert R[0, 0] == 0.965
     assert R[0, 1] == -0.057
@@ -17,7 +18,7 @@ def test_kolz():
     assert R[2, 1] == 0.171
     assert R[2, 2] == 0.950
 
-    R = get_kolz_rotation_matrix(Correction.SCAPULA_KOLZ_GLENOID_TO_PA_ROTATION)
+    R = get_kolz_rotation_matrix(Correction.SCAPULA_KOLZ_GLENOID_TO_PA_ROTATION, orthonormalize=False)
     assert R.shape == (3, 3)
     assert R[0, 0] == 0.949
     assert R[0, 1] == -0.056
@@ -29,6 +30,18 @@ def test_kolz():
     assert R[2, 1] == 0.200
     assert R[2, 2] == 0.928
 
+    R = get_kolz_rotation_matrix(Correction.SCAPULA_KOLZ_AC_TO_PA_ROTATION, orthonormalize=True)
+    assert R.shape == (3, 3)
+    np.testing.assert_almost_equal(R[0, 0], 9.62130083e-01)
+    np.testing.assert_almost_equal(R[0, 1], -5.77424614e-02)
+    np.testing.assert_almost_equal(R[0, 2], 2.66404789e-01)
+    np.testing.assert_almost_equal(R[1, 0], 5.56368185e-02)
+    np.testing.assert_almost_equal(R[1, 1], 9.98331511e-01)
+    np.testing.assert_almost_equal(R[1, 2], 1.54511560e-02)
+    np.testing.assert_almost_equal(R[2, 0], -2.66852483e-01)
+    np.testing.assert_almost_equal(R[2, 1], -4.41071341e-05)
+    np.testing.assert_almost_equal(R[2, 2], 9.63737387e-01)
+    np.testing.assert_almost_equal(np.linalg.det(R), 1.0)
+
     with pytest.raises(ValueError):
         get_kolz_rotation_matrix(correction=Correction.TO_ISB_ROTATION)
-        
