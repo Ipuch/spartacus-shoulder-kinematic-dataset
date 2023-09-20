f9929074232e15def6b687dc3196f09b1859eebf
Ipuch <pierre.puchaud@umontreal.ca>
2023-09-20 07:54:40 -0400

    renaming

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="shoulder",
+    name="spartacus",
     version="0.0.1",
     author="Ipuch, ANaaim, FMoissenet",
     author_email="",
     description="Shoulder dataset treatment",
     long_description=long_description,
-    packages=[".", "shoulder", "examples", "shoulder/dataset"],
+    packages=[".", "spartacus", "examples", "spartacus/dataset"],
     python_requires=">=3.10",
     zip_safe=False,
 )
--- spartacus/load_confident_data.py
+++ spartacus/load_confident_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from shoulder import (
+from . import (
     DatasetCSV,
     RowData,
 )
