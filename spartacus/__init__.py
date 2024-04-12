930bd5250c4758e63b02501ab69fdc7dee5da522
Ipuch <pierre.puchaud@umontreal.ca>
2024-04-12 10:25:48 -0400

    fix: restore init

--- spartacus/__init__.py
+++ spartacus/__init__.py
@@ -1,1 +1,26 @@
+from .src.enums import (
+    CartesianAxis,
+    EulerSequence,
+    JointType,
+    DatasetCSV,
+    BiomechDirection,
+    BiomechOrigin,
+    Segment,
+    DataFolder,
+)
+from .src.checks import (
+    check_parent_child_joint,
+    check_segment_filled_with_nan,
+    check_is_isb_segment,
+    check_is_euler_sequence_provided,
+    check_is_translation_provided,
+    check_same_orientation,
+)
 
+from .src.row_data import RowData
+from .src.load import load, Spartacus
+from .src.utils import compute_rotation_matrix_from_axes, flip_rotations
+from .src.joint import Joint
+from .src.biomech_system import BiomechCoordinateSystem
+from .src.checks import check_same_orientation
+from .plots.quick_load import import_data
