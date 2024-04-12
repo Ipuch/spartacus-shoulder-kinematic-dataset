7171cda593a33ee8f09147637a7b3b688c0729ab
Ipuch <pierre.puchaud@umontreal.ca>
2024-04-12 10:36:50 -0400

    tests: fixed

--- examples/first_example.py
+++ examples/first_example.py
@@ -5,7 +5,7 @@ def main():
     spartacus_dataset = sp.load()
     print(spartacus_dataset.confident_data_values)
     spartacus_dataset.export()
-    return spartacus_dataset.confident_data_values
+    return spartacus_dataset.corrected_confident_data_values
 
 
 if __name__ == "__main__":
