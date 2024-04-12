97081ac37d2f1137ee1e1286ddea910a8ea39fe6
Ipuch <pierre.puchaud@umontreal.ca>
2024-04-12 03:48:49 -0400

    fix: dash dataset

--- spartacus/src/plots/plot_data.py
+++ spartacus/src/plots/plot_data.py
@@ -1,76 +1,34 @@
-# test
-# http://127.0.0.1:8050/
-
-from dash import Dash, dcc, html, Input, Output, State, callback
-import plotly.express as px
-import pandas as pd
-import io
 import base64
-from spartacus import load
+import io
+import os
+import pandas as pd
+import plotly.express as px
+import webbrowser
+from dash import Dash, dcc, html, Input, Output, State, callback
+from pathlib import Path
 
+from spartacus import DatasetCSV
+from spartacus import load
 
 # TODO : Put the correct joint in the article.
 # TODO : Add a curve directly from the app running
 # TODO : Update graph when data is added.
 # TODO : check for name (flexion extension abduction adduction etc.. with positive and negative value + adapted name if needed for specific joint)
-# Question à aborder sur la forme des données.
-extracted_data = load().import_confident_data()
-extracted_data.angle_translation = "angle"
 # TODO : do a function to change the name of the degree of freedom
 # Todo : Change the name of the function to be more clean ==> not draft anymore.
-# Begin to check curves for outlier.
-for i in range(extracted_data.degree_of_freedom.size):
-    if extracted_data.degree_of_freedom[i] == "1":
-        extracted_data.degree_of_freedom[i] = "flexion"
-    elif extracted_data.degree_of_freedom[i] == "2":
-        extracted_data.degree_of_freedom[i] = "abduction"
-    elif extracted_data.degree_of_freedom[i] == "3":
-        extracted_data.degree_of_freedom[i] = "external_rotation"
+
 app = Dash(__name__)
 
-app.layout = html.Div(
-    [  # Global Title of the graph
-        html.H4("Kinematics of the shoulder joint"),
-        html.Button("Download CSV", id="btn_csv"),
-        dcc.Download(id="download-dataframe-csv"),
-        # Plot the graph
-        dcc.Graph(id="graph"),
-        # Show the different options in different collumn
-        dcc.Dropdown(
-            id="humeral_motion",
-            options=sorted([i for i in extracted_data.humeral_motion.unique()]),
-            value=sorted([i for i in extracted_data.humeral_motion.unique()])[0],
-        ),
-        dcc.Checklist(
-            id="joint",
-            options=sorted([i for i in extracted_data.joint.unique()]),
-            value=sorted([i for i in extracted_data.joint.unique()]),
-            inline=True,
-        ),
-        dcc.Dropdown(
-            options=sorted([i for i in extracted_data.angle_translation.unique()]),
-            value=sorted([i for i in extracted_data.angle_translation.unique()])[0],
-            id="angle_translation",
-        ),
-        dcc.Upload(
-            id="upload-data",
-            children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
-            style={
-                "width": "100%",
-                "height": "60px",
-                "lineHeight": "60px",
-                "borderWidth": "1px",
-                "borderStyle": "dashed",
-                "borderRadius": "5px",
-                "textAlign": "center",
-                "margin": "10px",
-            },
-            # Allow multiple files to be uploaded
-            multiple=True,
-        ),
-        html.Div(id="output"),
-    ]
-)
+
+def import_data():
+    """Import the data from the confident_data.csv file if it exists, otherwise its computed from the raw data."""
+    if "confident_data.csv" in os.listdir(str(Path(DatasetCSV.CLEAN.value).parent)):
+        return pd.read_csv(Path(DatasetCSV.CLEAN.value).parent / "confident_data.csv")
+    else:
+        return load().import_confident_data()
+
+
+extracted_data = import_data()
 
 
 # Import data
@@ -100,16 +58,16 @@ def update_output(contents):
     Output("download-dataframe-csv", "data"),
     State("humeral_motion", "value"),
     State("joint", "value"),
-    State("angle_translation", "value"),
+    State("unit", "value"),
     Input("btn_csv", "n_clicks"),
     prevent_initial_call=True,
 )
-def export_data(humeral_motion, joint, angle_translation, n_clicks):
+def export_data(humeral_motion, joint, unit, n_clicks):
     df = extracted_data  # replace with your own data source
     mask_joint = df.joint.isin(joint)
     mask_mvt = df.humeral_motion.isin([humeral_motion])
     # We have to put Angle translation in a list because it is a string
-    mask_angle_translation = df.angle_translation.isin([angle_translation])
+    mask_angle_translation = df.unit.isin([unit])
     # In order to have the data in the correct orger we have to define a list ordering the data
     list_joint_graph_base_in_order = ["humerothoracic", "glenohumeral", "scapulothoracic", "acromioclavicular"]
 
@@ -121,14 +79,14 @@ def export_data(humeral_motion, joint, angle_translation, n_clicks):
     Output("graph", "figure"),
     Input("humeral_motion", "value"),
     Input("joint", "value"),
-    Input("angle_translation", "value"),
+    Input("unit", "value"),
 )
-def update_line_chart(humeral_motion, joint, angle_translation):
+def update_line_chart(humeral_motion, joint, unit):
     df = extracted_data  # replace with your own data source
     mask_joint = df.joint.isin(joint)
     mask_mvt = df.humeral_motion.isin([humeral_motion])
     # We have to put Angle translation in a list because it is a string
-    mask_angle_translation = df.angle_translation.isin([angle_translation])
+    mask_angle_translation = df.unit.isin([unit])
     # In order to have the data in the correct orger we have to define a list ordering the data
     list_joint_graph_base_in_order = ["humerothoracic", "glenohumeral", "scapulothoracic", "acromioclavicular"]
     # Adapt the list to the number of degree of freedom selectionned by the user.
@@ -137,10 +95,6 @@ def update_line_chart(humeral_motion, joint, angle_translation):
         if name_joint in joint:
             list_to_plot_in_order.append(name_joint)
 
-    if angle_translation == "angle":
-        list_orga = ["flexion", "abduction", "external_rotation"]
-    elif angle_translation == "translation":
-        list_orga = ["X", "Y", "Z"]
     fig = px.scatter(
         df[mask_mvt & mask_joint & mask_angle_translation],
         x="humerothoracic_angle",
@@ -148,8 +102,10 @@ def update_line_chart(humeral_motion, joint, angle_translation):
         color="article",
         facet_row="joint",
         facet_col="degree_of_freedom",
-        category_orders={"degree_of_freedom": list_orga, "joint": list_to_plot_in_order},
     )
+    for i in range(4):
+        fig.update_yaxes(title_text="Angle (rad)" if unit == "rad" else "Translation (mm)", row=i + 1, col=1)
+
     # Allow to remove the "Mvt=" in the legend
     fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
     # here to switch between different layout
@@ -179,5 +135,59 @@ def update_line_chart(humeral_motion, joint, angle_translation):
     return fig
 
 
+def launch_app(data):
+
+    app.layout = html.Div(
+        [  # Global Title of the graph
+            html.H4("Kinematics of the shoulder joint"),
+            html.Button("Download CSV", id="btn_csv"),
+            dcc.Download(id="download-dataframe-csv"),
+            # Plot the graph
+            dcc.Graph(id="graph"),
+            # Show the different options in different collumn
+            dcc.Dropdown(
+                id="humeral_motion",
+                options=sorted([i for i in data.humeral_motion.unique()]),
+                value=sorted([i for i in data.humeral_motion.unique()])[0],
+            ),
+            dcc.Checklist(
+                id="joint",
+                options=sorted([i for i in data.joint.unique()]),
+                value=sorted([i for i in data.joint.unique()]),
+                inline=True,
+            ),
+            dcc.Dropdown(
+                options=sorted([i for i in data.unit.unique()]),
+                value=sorted([i for i in data.unit.unique()])[0],
+                id="unit",
+            ),
+            dcc.Upload(
+                id="upload-data",
+                children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
+                style={
+                    "width": "100%",
+                    "height": "60px",
+                    "lineHeight": "60px",
+                    "borderWidth": "1px",
+                    "borderStyle": "dashed",
+                    "borderRadius": "5px",
+                    "textAlign": "center",
+                    "margin": "10px",
+                },
+                # Allow multiple files to be uploaded
+                multiple=True,
+            ),
+            html.Div(id="output"),
+        ]
+    )
+
+
+def main():
+    extracted_data = import_data()
+    launch_app(extracted_data)
+
+
 if __name__ == "__main__":
+    main()
     app.run_server(debug=True)
+    webbrowser.open("http://127.0.0.1:8050/")
