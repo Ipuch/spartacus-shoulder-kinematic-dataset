7f811c2927661ec97e7c5847a90be072519a9264
Ipuch <pierre.puchaud@umontreal.ca>
2024-04-12 10:25:18 -0400

    again

--- spartacus/plots/legend_utils.py
+++ spartacus/plots/legend_utils.py
@@ -1,24 +1,5 @@
-from .joint import JointType
+from .constants import BIOMECHANICAL_DOF_LEGEND
 
 
-def isb_rotation_biomechanical_dof(joint_type: JointType):
-    joint_mapping = {
-        JointType.GLENO_HUMERAL: ("plane of elevation", "elevation", "internal(+)-external(-) rotation"),
-        JointType.SCAPULO_THORACIC: (
-            "protraction(+)-retraction(-)",
-            "medial(+)-lateral(-) rotation",
-            "posterior(+)-anterior(-) tilt",
-        ),
-        JointType.ACROMIO_CLAVICULAR: (
-            "protraction(+)/retraction(-)",
-            "medial(+)/lateral(-) rotation",
-            "posterior(+)/anterior(-) tilt",
-        ),
-        JointType.STERNO_CLAVICULAR: (
-            "protraction(+)/retraction(-)",
-            "depression(+)/elevation(-)",
-            "backwards(+)/forward(-) rotation",
-        ),
-        JointType.THORACO_HUMERAL: ("plane of elevation", "elevation", "internal(+)/external(-) rotation"),
-    }
-    return joint_mapping.get(joint_type)
+def isb_rotation_biomechanical_dof(joint_type: str):
+    return BIOMECHANICAL_DOF_LEGEND.get(joint_type)
