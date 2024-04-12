5011f871729aa0e8a6d1e934fb3801c702820ab8
Ipuch <pierre.puchaud@umontreal.ca>
2024-04-12 19:40:17 -0400

    feature: yeah graph works :)

--- examples/static_plot.py
+++ examples/static_plot.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from pandas import DataFrame
 from plotly import graph_objs as go
 import plotly.express as px
@@ -39,129 +41,149 @@ class DataFrameInterface:
     def nb_dof(self) -> int:
         return self.df["degree_of_freedom"].nunique()
 
+    def select_motion(self, motion: str) -> DataFrame:
+        return self.df[self.df["humeral_motion"] == motion]
+
+    def select_article(self, article: str) -> DataFrame:
+        return self.df[self.df["article"] == article]
+
+    def select_joint(self, joint: str) -> DataFrame:
+        return self.df[self.df["joint"] == joint]
+
+    def plot_motion(self, motion: str):
+        pass
+
+
+class DataPlanchePlotting:
+    def __init__(self, dfi: DataFrameInterface):
+        self.dfi = dfi
+        self.fig = make_subplots(
+            shared_xaxes=False,
+            shared_yaxes=False,
+            rows=4,
+            cols=3,
+            subplot_titles=self._rotation_titles,
+        )
+        self.showlegend = True
+
+    @property
+    def _rotation_titles(self) -> list[str]:
+        suplot_titles = [list(v) for _, v in BIOMECHANICAL_DOF_LEGEND.items()]
+        return [item for sublist in suplot_titles for item in sublist]
+
+    def plot(self):
+        for article in self.dfi.df["article"].unique():
+            self.showlegend = True
+            self.plot_article(name=article)
+
+    def plot_article(self, name):
+        sub_dfi = DataFrameInterface(self.dfi.select_article(article=name))
+        for j, joint in enumerate(BIOMECHANICAL_DOF_LEGEND.keys()):
+            sub_df_j = sub_dfi.select_joint(joint)
+
+            if sub_df_j.empty:
+                continue
+
+            self.plot_dofs(article=name, joint=joint)
+
+    def plot_dofs(self, article, joint):
+        sub_dfi = DataFrameInterface(self.dfi.select_article(article=article))
+        sub_df_j = sub_dfi.select_joint(joint)
+
+        dofs = sub_df_j["degree_of_freedom"].unique()
+
+        for i, dof in enumerate(dofs):
+            self.plot_dof(article, joint, dof)
+
+    def plot_dof(self, article, joint, dof):
+        sub_dfi = DataFrameInterface(self.dfi.select_article(article=article))
+        sub_df_j = sub_dfi.select_joint(joint)
+        sub_df_ij = sub_df_j[sub_df_j["degree_of_freedom"] == dof]
+        row, col = JOINT_ROW_COL_INDEX[joint][dof - 1]
+        subjects = sub_df_ij["shoulder_id"].unique()
+
+        if len(subjects) > 1:
+            for s in subjects:
+                sub_df_ij_s = sub_df_ij[sub_df_ij["shoulder_id"] == s]
+                self.plot_timeserie(sub_df_ij_s, article, row, col)
+        else:
+            self.plot_timeserie(sub_df_ij, article, row, col)
+
+        row, col_left = JOINT_ROW_COL_INDEX[joint][0]
+        self.fig.update_yaxes(title_text=f"{joint[0].upper()}{joint[1:].lower()}", row=row + 1, col=col_left + 1)
+
+    def plot_timeserie(self, df, article, row, col):
+        self.fig.add_trace(
+            go.Scatter(
+                x=df["humerothoracic_angle"],
+                y=df["value"],
+                name=article,
+                legendgroup=article,
+                showlegend=self.showlegend,
+                # legend=article_0,
+                mode="lines+markers",
+                opacity=0.5,
+                marker=dict(
+                    size=2,
+                    color=f"rgba{AUTHORS_COLORS[article]}",
+                    # line=dict(width=0.5),
+                ),
+                line=dict(
+                    width=0.5,
+                    color=f"rgba{AUTHORS_COLORS[article]}",
+                ),
+            ),
+            row=row + 1,
+            col=col + 1,
+        )
+        self.showlegend = False
+
+    def update_style(self):
+        self.fig.update_layout(
+            # If we fix only the height the width will be adapted to the size of the screen
+            # However not fixing the height AND the width make the graph not readable
+            height=1000,
+            width=1000,
+            paper_bgcolor="rgba(255,255,255,1)",
+            plot_bgcolor="rgba(255,255,255,1)",
+            legend=dict(
+                title_font_family="Times New Roman",
+                font=dict(family="Times New Roman", color="black", size=16),
+                orientation="v",
+                x=1,
+                y=1,
+            ),
+            font=dict(
+                size=16,
+                family="Times New Roman",
+            ),
+            yaxis=dict(color="black"),
+            template="simple_white",
+            boxgap=0.1,
+        )
+
+    def show(self):
+        self.fig.show()
+
 
 def main():
 
     df = import_data(correction=False)
     # df = import_data(correction=True)
 
-    sub_df = df[df["humeral_motion"] == "frontal elevation"]
-    dfi = DataFrameInterface(sub_df)
-    # BIOMECHANICAL_DOF_LEGEND, turn values of dict as list, breaking tuples
-    suplot_titles = [list(v) for _, v in BIOMECHANICAL_DOF_LEGEND.items()]
-    suplot_titles = [item for sublist in suplot_titles for item in sublist]
+    humeral_motions = df["humeral_motion"].unique()
 
-    fig = make_subplots(
-        shared_xaxes=False,
-        shared_yaxes=False,
-        rows=dfi.nb_joints,
-        cols=dfi.nb_dof,
-        subplot_titles=suplot_titles,
-    )
-
-    article_0 = dfi.df["article"].unique()[0]
-    sub_df = dfi.df[dfi.df["article"] == article_0]
-    sub_dfi = DataFrameInterface(sub_df)
-    legend_showed = True
-    for i, joint in enumerate(BIOMECHANICAL_DOF_LEGEND.keys()):
-        sub_df_i = sub_df[sub_df["joint"] == joint]
-
-        if sub_df_i.empty:
-            continue
-
-        dofs = sub_df_i["degree_of_freedom"].unique()
-
-        for j, dof in enumerate(dofs):
-            sub_df_ij = sub_df_i[sub_df_i["degree_of_freedom"] == dof]
-            row, col = JOINT_ROW_COL_INDEX[joint][dof - 1]
-
-            subjects = sub_df_ij["shoulder_id"].unique()
-            if subjects.size == 0:
-                fig.add_trace(
-                    go.Scatter(
-                        x=sub_df_ij["humerothoracic_angle"],
-                        y=sub_df_ij["value"],
-                        name=article_0,
-                        legendgroup=article_0,
-                        showlegend=legend_showed,
-                        # legend=article_0,
-                        mode="lines+markers",
-                        opacity=0.5,
-                        marker=dict(
-                            size=2,
-                            color=f"rgba{AUTHORS_COLORS[article_0]}",
-                            # line=dict(width=0.5),
-                        ),
-                        line=dict(
-                            width=0.5,
-                            color=f"rgba{AUTHORS_COLORS[article_0]}",
-                        ),
-                    ),
-                    row=row + 1,
-                    col=col + 1,
-                )
-                legend_showed = False
-            else:
-                for s in subjects[0]:
-                    # for s in subjects:
-                    sub_df_ij_s = sub_df_ij[sub_df_ij["shoulder_id"] == s]
-                    fig.add_trace(
-                        go.Scatter(
-                            x=sub_df_ij_s["humerothoracic_angle"],
-                            y=sub_df_ij_s["value"],
-                            name=article_0,
-                            legendgroup=article_0,
-                            showlegend=legend_showed,
-                            # legend=article_0,
-                            mode="lines+markers",
-                            opacity=0.5,
-                            marker=dict(
-                                size=2,
-                                color=f"rgba{AUTHORS_COLORS[article_0]}",
-                                # line=dict(width=0.5),
-                            ),
-                            line=dict(
-                                width=0.5,
-                                color=f"rgba{AUTHORS_COLORS[article_0]}",
-                            ),
-                        ),
-                        row=row + 1,
-                        col=col + 1,
-                    )
-                    legend_showed = False
-
-        row, col_left = JOINT_ROW_COL_INDEX[joint][0]
-        fig.update_yaxes(title_text=f"{joint[0].upper()}{joint[1:].lower()}", row=row + 1, col=col_left + 1)
-        # fig.update_traces(
-        #     #     markersize
-        #     # marker=dict(size=1, opacity=0.3),
-        #     # line=dict(width=1, opacity=0.3),
-        # )
-
-    fig.update_layout(
-        # If we fix only the height the width will be adapted to the size of the screen
-        # However not fixing the height AND the width make the graph not readable
-        height=1000,
-        width=1000,
-        paper_bgcolor="rgba(255,255,255,1)",
-        plot_bgcolor="rgba(255,255,255,1)",
-        legend=dict(
-            title_font_family="Times New Roman",
-            font=dict(family="Times New Roman", color="black", size=16),
-            orientation="v",
-            x=1,
-            y=1,
-        ),
-        font=dict(
-            size=16,
-            family="Times New Roman",
-        ),
-        yaxis=dict(color="black"),
-        template="simple_white",
-        boxgap=0.1,
-    )
-
-    fig.show()
+    for mvt in humeral_motions:
+        sub_df = df[df["humeral_motion"] == mvt]
+        dfi = DataFrameInterface(sub_df)
+        plt = DataPlanchePlotting(dfi)
+        plt.plot()
+        # plt.plot_article(name="Bourne 2003")
+        plt.update_style()
+        plt.show()
+        plt.fig.write_image(f"{mvt}.png")
+        plt.fig.write_image(f"{mvt}.pdf")
+        plt.fig.write_html(f"{mvt}.html", include_mathjax="cdn")  #
 
 
 if __name__ == "__main__":
