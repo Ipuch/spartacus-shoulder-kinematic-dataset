cb597a305a895e82f282fb125432d3f01529511f
Ipuch <pierre.puchaud@umontreal.ca>
2024-05-15 16:39:32 -0400

    fix: update paths and dataset names
    todo

--- spartacus/src/enums.py
+++ spartacus/src/enums.py
@@ -11,58 +11,65 @@ class DatasetCSV(Enum):
 
 
 class DataFolder(Enum):
-    BOURNE_2003 = Path(__file__).parent.parent / "data" / "Bourne 2003"
-    CEREATTI_2017 = Path(__file__).parent.parent / "data" / "Cereatti et al 2017" / "S2M"
-    CHARBONNIER_2014 = Path(__file__).parent.parent / "data" / "Charbonnier et al 2014"
-    CHU_2012 = Path(__file__).parent.parent / "data" / "Chu et al 2012"
-    DAL_MASO_2014 = Path(__file__).parent.parent / "data" / "Dal Maso et al 2014"
-    FUNG_2001 = Path(__file__).parent.parent / "data" / "Fung et al 2001"
-    GUTIERREZ_DELGADO_2017 = Path(__file__).parent.parent / "data" / "Gutierrez Delgado et al 2017"
-    KIJIMA_2015 = Path(__file__).parent.parent / "data" / "Kijima et al 2015"
-    KIM_2017 = Path(__file__).parent.parent / "data" / "Kim et al 2017"
+    BOURNE_2003 = Path(__file__).parent.parent / "data" / "#2_Bourne_et_al"
+    CHU_2012 = Path(__file__).parent.parent / "data" / "#3_Chu_et_al"
+    FUNG_2001 = Path(__file__).parent.parent / "data" / "#4_Fung_et_al"
+    GUTIERREZ_DELGADO_2017 = Path(__file__).parent.parent / "data" / "#5_Gutierrez_Delgado_et_al"
     KOLZ_2020 = Path(__file__).parent.parent / "data" / "Kolz et al 2020"
-    KONOZO_2017 = Path(__file__).parent.parent / "data" / "Kozono et al 2017"
-    LAWRENCE_2014 = Path(__file__).parent.parent / "data" / "Lawrence et al 2014"
-    MATSUKI_2011 = Path(__file__).parent.parent / "data" / "Matsuki et al 2011"
-    MATSUKI_2012 = Path(__file__).parent.parent / "data" / "Matsuki et al 2012"
-    MATSUKI_2014 = Path(__file__).parent.parent / "data" / "Matsuki et al 2014"
-    MATSUMURA_2013 = Path(__file__).parent.parent / "data" / "Matsumura et al 2013"
-    MCCLURE_2001 = Path(__file__).parent.parent / "data" / "McClure et al 2001"
-    NISHINAKA_2008 = Path(__file__).parent.parent / "data" / "Nishinaka et al 2008"
-    OKI_2012 = Path(__file__).parent.parent / "data" / "Oki et al 2012"
-    SAHARA_2006 = Path(__file__).parent.parent / "data" / "Sahara et al 2006"
-    SAHARA_2007 = Path(__file__).parent.parent / "data" / "Sahara et al 2007"
-    SUGI_2021 = Path(__file__).parent.parent / "data" / "Sugi et al 2021"
-    TEECE_2008 = Path(__file__).parent.parent / "data" / "Teece et al 2008"
-    YOSHIDA_2023 = Path(__file__).parent.parent / "data" / "Yoshida et al 2023"
+    MCCLURE_2001 = Path(__file__).parent.parent / "data" / "#7_Karduna_et_al"
+    KIJIMA_2015 = Path(__file__).parent.parent / "data" / "#8_Kijima_et_al"
+    KIM_2017 = Path(__file__).parent.parent / "data" / "#9_Kim_et_al"
+    KONOZO_2017 = Path(__file__).parent.parent / "data" / "#10_Kozono_et_al"
+    LAWRENCE_2014 = Path(__file__).parent.parent / "data" / "#11_Ludwig_et_al"
+    MATSUKI_2011 = Path(__file__).parent.parent / "data" / "#12_Matsuki_et_al"
+    # MATSUKI_2011 = Path(__file__).parent.parent / "data" / "Matsuki et al 2011"
+    # MATSUKI_2012 = Path(__file__).parent.parent / "data" / "Matsuki et al 2012"
+    # MATSUKI_2014 = Path(__file__).parent.parent / "data" / "Matsuki et al 2014"
+    MATSUMURA_2013 = Path(__file__).parent.parent / "data" / "#13_Matsumara_et_al"
+    MOISSENET = Path(__file__).parent.parent / "data" / "#14_Moissenet_et_al"
+    NISHINAKA_2008 = Path(__file__).parent.parent / "data" / "#15_Nishinaka_et_al"
+    OKI_2012 = Path(__file__).parent.parent / "data" / "#16_Oki_et_al"
+    SAHARA_2006 = Path(__file__).parent.parent / "data" / "#17_Sahara_et_al"
+    # SAHARA_2006 = Path(__file__).parent.parent / "data" / "Sahara et al 2006"
+    # SAHARA_2007 = Path(__file__).parent.parent / "data" / "Sahara et al 2007"
+    SUGI_2021 = Path(__file__).parent.parent / "data" / "#18_Sugi_et_al"
+    TEECE_2008 = Path(__file__).parent.parent / "data" / "#19_Teece_et_al"
+    YOSHIDA_2023 = Path(__file__).parent.parent / "data" / "#20_Yoshida_et_al"
+    CEREATTI_2017 = Path(__file__).parent.parent / "data" / "Cereatti et al 2017" / "S2M"
+    DAL_MASO_2014 = Path(__file__).parent.parent / "data" / "Dal Maso et al 2014"
+    MALBERG = "TODO"
 
     @classmethod
     def from_string(cls, data_folder: str):
         folder_name_to_enum = {
-            "Bourne 2003": cls.BOURNE_2003,
             "Cereatti et al 2017/S2M": cls.CEREATTI_2017,
-            "Charbonnier et al 2014": cls.CHARBONNIER_2014,
-            "Chu et al 2012": cls.CHU_2012,
             "Dal Maso et al 2014": cls.DAL_MASO_2014,
-            "Fung et al 2001": cls.FUNG_2001,
-            "Gutierrez Delgado et al 2017": cls.GUTIERREZ_DELGADO_2017,
-            "Kijima et al 2015": cls.KIJIMA_2015,
-            "Kim et al 2017": cls.KIM_2017,
-            "Kolz et al 2020": cls.KOLZ_2020,
-            "Kozono et al 2017": cls.KONOZO_2017,
-            "Lawrence et al 2014": cls.LAWRENCE_2014,
-            "Matsuki et al 2011": cls.MATSUKI_2011,
-            "Matsuki et al 2012": cls.MATSUKI_2012,
-            "Matsuki et al 2014": cls.MATSUKI_2014,
-            "Matsumura et al 2013": cls.MATSUMURA_2013,
-            "McClure et al 2001": cls.MCCLURE_2001,
-            "Nishinaka et al 2008": cls.NISHINAKA_2008,
-            "Oki et al 2012": cls.OKI_2012,
-            "Sahara et al 2006": cls.SAHARA_2006,
-            "Sahara et al 2007": cls.SAHARA_2007,
-            "Sugi et al 2021": cls.SUGI_2021,
-            "Teece et al 2008": cls.TEECE_2008,
-            "Yoshida et al 2023": cls.YOSHIDA_2023,
+            # "Bourne 2003": cls.BOURNE_2003,
+            "#2_Bourne_et_al": cls.BOURNE_2003,
+            "#3_Chu_et_al": cls.CHU_2012,  # "Chu et al 2012"
+            "#4_Fung_et_al": cls.FUNG_2001,  # "Fung et al 2001"
+            "#5_Gutierrez_Delgado_et_al": cls.GUTIERREZ_DELGADO_2017,  # "Gutierrez Delgado et al 2017"
+            "Kolz et al 2020": cls.KOLZ_2020,  # "Kolz et al 2020
+            "#7_Karduna_et_al": cls.MCCLURE_2001,
+            "#8_Kijima_et_al": cls.KIJIMA_2015,  # "Kijima et al 2015"
+            "#9_Kim_et_al": cls.KIM_2017,  # "Kim et al 2017"
+            "#10_Kozono_et_al": cls.KONOZO_2017,  # "Kozono et al 2017"
+            "#11_Ludwig_et_al": cls.LAWRENCE_2014,
+            "#12_Matsuki_et_al": cls.MATSUKI_2011,  # "Matsuki et al 2011"
+            # "Matsuki et al 2011": cls.MATSUKI_2011,
+            # "Matsuki et al 2012": cls.MATSUKI_2012,
+            # "Matsuki et al 2014": cls.MATSUKI_2014,
+            "#13_Matsumara_et_al": cls.MATSUMURA_2013,  # "Matsumura et al 2013"
+            "#14_Moissenet_et_al": cls.MOISSENET,  # "Moissenet et al"
+            "#15_Nishinaka_et_al": cls.NISHINAKA_2008,  # "Nishinaka et al 2008"
+            "#16_Oki_et_al": cls.OKI_2012,  # "Oki et al 2012"
+            "#17_Sahara_et_al": cls.SAHARA_2006,  # "Sahara et al 2006"
+            # "Sahara et al 2006": cls.SAHARA_2006,
+            # "Sahara et al 2007": cls.SAHARA_2007,
+            "#18_Sugi_et_al": cls.SUGI_2021,  # "Sugi et al 2021"
+            "#19_Teece_et_al": cls.TEECE_2008,  # "Teece et al 2008"
+            "#20_Yoshida_et_al": cls.YOSHIDA_2023,  # "Yoshida et al 2023"
+            "#XX_Malberg": cls.MALBERG,
         }
 
         the_enum = folder_name_to_enum.get(data_folder)
